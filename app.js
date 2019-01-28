ecb395856cf0139cac79d4d7621a309ea0fdef08
Trey Arnold <arnold.trey80@gmail.com>
2019-01-28 09:26:27 -0600

    add 2nd app.listen for local testing

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.get("/", function(req, res){
     res.render("home.ejs")
 });
 
+// Local server startup for testing
+// app.listen(3000, function(){
+// hosted server startup for deployment
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server has started");
 });
